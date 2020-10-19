53360f8c207784fca5ec2cdae47cd864810bfd89
miok-jung <j-narin@naver.com>
2020-10-19 13:52:04 +0900

    메인바디테스트

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React from "react";
-
+import MainContainer from './MainContainer';
 function App() {
-  return <div>App</div>;
+  return (
+    <div>
+      <MainContainer />
+    </div>
+  );
 }
 
 export default App;
