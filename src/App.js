91b440c240c810c2ca1ac43229e62d3e20872193
smilejakdu <ash982416@gmail.com>
2020-10-31 19:23:19 +0900

    signup-form

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import MainPage from "./pages/MainPage";
+import MainPage from "./pages/MainPage/MainPage";
+import SignupPage from "./pages/SignupPage/SignupPage";
 
 function App() {
   return (
     <div>
       <Router>
         <Route path="/" exact="true" component={MainPage}></Route>
+        <Route path="/signup" exact="true" component={SignupPage}></Route>
       </Router>
     </div>
   );
